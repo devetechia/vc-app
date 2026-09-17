bcc386fcd6a8fceacb959deee673bb8be3d34175
vistaprods-projects <vistaprod.io@gmail.com>
2026-09-17 16:54:59 +0200

    Fix: remove lPAHis7WqXw from NO_TRANSCRIPT_IDS (transcript now available)

--- clases/script.js
+++ clases/script.js
@@ -838,7 +838,7 @@ function renderTitleWithSeparators(title) {
 }
 
 // ===== SERMON BADGE HELPER =====
-const NO_TRANSCRIPT_IDS = ['lPAHis7WqXw', 'MgHTULjQdJI', 'az_L2h47m8Q'];
+const NO_TRANSCRIPT_IDS = ['MgHTULjQdJI', 'az_L2h47m8Q'];
 
 function getSermonBadges(videoId, index, append) {
     const isWatched = isVideoWatched(videoId);
